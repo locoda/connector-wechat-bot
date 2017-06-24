fd1c1f5a138955bbd885ae4bea86f99d2c368036
locoda <locoda@foxmail.com>
2017-06-24 22:31:24 +0800

    rewrite into python3, well, nothing changed

--- wechat-bot/messages.py
+++ wechat-bot/messages.py
@@ -10,51 +10,51 @@ import apiai
 from api_tokens import *
 
 def tuling_reply(msg_content, user_id):
-    url_api = 'http://www.tuling123.com/openapi/api'
-    data = {
-        'key'    : TULING_TOKEN,
-        'info'   : msg_content,
-        'userid' : user_id,
-    }
-    
-    print "use Tuling reply"
-    s = requests.post(url_api, data=data).json()
-    print 'return code: ' + str(s['code'])
-    if s['code'] == 100000:
+	url_api = 'http://www.tuling123.com/openapi/api'
+	data = {
+		'key'	: TULING_TOKEN,
+		'info'   : msg_content,
+		'userid' : user_id,
+	}
+	
+	print("use Tuling reply")
+	s = requests.post(url_api, data=data).json()
+	print('return code: ' + str(s['code']))
+	if s['code'] == 100000:
 		return s['text']
-    if s['code'] == 200000:
-        return s['text'] + s['url']
-    if s['code'] == 302000:
-        news = random.choice(s['list'])
-        return news['article'] + '\n' + news['detailurl']
-    if s['code'] == 308000:
-        menu = random.choice(s['list'])
-        return menu['name'] + '\n' + menu['detailurl'] + '\n' + menu['info']
+	if s['code'] == 200000:
+		return s['text'] + s['url']
+	if s['code'] == 302000:
+		news = random.choice(s['list'])
+		return news['article'] + '\n' + news['detailurl']
+	if s['code'] == 308000:
+		menu = random.choice(s['list'])
+		return menu['name'] + '\n' + menu['detailurl'] + '\n' + menu['info']
 
 
 def apiai_reply(msg_content, user_id):
-    print "try APIAI reply..."
-    ai = apiai.ApiAI(APIAI_TOKEN)
-    request = ai.text_request()
-    request.lang = 'zh-CN'
-    request.session_id = user_id
-    request.query = msg_content
+	print("try APIAI reply...")
+	ai = apiai.ApiAI(APIAI_TOKEN)
+	request = ai.text_request()
+	request.lang = 'zh-CN'
+	request.session_id = user_id
+	request.query = msg_content
 
-    response = request.getresponse()
-    s = json.loads(response.read(), encoding='UTF-8')
-    
-    if s['result']['action'] == 'input.unknown':
-        raise Exception('api.ai cannot reply this message')
-    if s['status']['code'] == 200:
-        print "use APIAI reply"
-        print 'return code: ' + str(s['status']['code'])
-        return s['result']['fulfillment']['speech']
+	response = request.getresponse()
+	s = json.loads(response.read().decode('utf-8'))
+	
+	if s['result']['action'] == 'input.unknown':
+		raise Exception('api.ai cannot reply this message')
+	if s['status']['code'] == 200:
+		print("use APIAI reply")
+		print('return code: ' + str(s['status']['code']))
+		return s['result']['fulfillment']['speech']
 
 
 def emotions_reply(keyword):
-    print "try gif reply..."
-    res = requests.get('https://www.doutula.com/search', {'keyword': keyword})
-    html = etree.HTML(res.text)
-    url = 'http:' + random.choice(html.xpath('//div[@class="image-container"][1]//img[contains(@class, "img-responsive")]/@data-original'))
+	print("try gif reply...")
+	res = requests.get('https://www.doutula.com/search', {'keyword': keyword})
+	html = etree.HTML(res.text)
+	url = 'http:' + random.choice(html.xpath('//div[@class="image-container"][1]//img[contains(@class, "img-responsive")]/@data-original'))
 
-    return url
+	return url
--- wechat-bot/wxbot.py
+++ wechat-bot/wxbot.py
@@ -16,24 +16,33 @@ bot.enable_puid()
 extensions = ['.jpg', '.png', '.gif']
 
 def send_online_notification(name):
-    my_friend = ensure_one(bot.search(name))
-    while True:
-        my_friend.send('I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime()))
-        time.sleep(3600)
+	my_friend = ensure_one(bot.search(name))
+	while True:
+		my_friend.send('I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime()))
+		time.sleep(3600)
 
 
-@bot.register()
+@bot.register(bot.self)
 def reply(msg):
 	if msg.text == '1':
 		return 'I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime())
 	else:
-		return tuling_reply(content, msg.sender.puid)
+		return tuling_reply(msg.text, msg.sender.puid)
 
+
+@bot.register(Group, SYSTEM, except_self=False)
+def incoming_student(msg):
+	# print('haha')
+	print(msg)
+	print(msg.raw)
+	# if u'加入' in msg.text:
+	# 	msg.reply(tuling_reply('welcome'.encode('UTF-8'), msg.member.puid))
+	# 	msg.reply_image('welcome.jpg')
 
 @bot.register(Group, TEXT)
 def group_msg(msg):
 	if msg.is_at:
-		content = re.sub('@[^\s]*', '', unicodedata.normalize('NFKC', msg.text)).strip().encode('utf-8')
+		content = re.sub('@[^\s]*', '', unicodedata.normalize('NFKC', msg.text)).strip()
 
 		if content.endswith(tuple(extensions)):
 			try:
@@ -46,17 +55,16 @@ def group_msg(msg):
 
 				msg.reply_image('.gif', media_id=media_id)
 			except Exception as error:
-				print error
-				msg.reply(u"本机器人没有找到相关表情~使用文字回复：\n" + tuling_reply(content, msg.member.puid))
+				print(error)
+				msg.reply("本机器人没有找到相关表情~使用文字回复：\n" + tuling_reply(content, msg.member.puid))
 		else:
 			try:
 				msg.reply(apiai_reply(content, msg.member.puid))
 			except Exception as error:
-				print error
+				print(error)
 				msg.reply(tuling_reply(content, msg.member.puid))
 
-positiveSendingThread = threading.Thread(target=send_online_notification,
-    args=(u'乙醚。',))
+positiveSendingThread = threading.Thread(target=send_online_notification, args=('乙醚。',))
 positiveSendingThread.setDaemon(True)
 positiveSendingThread.start()
 
