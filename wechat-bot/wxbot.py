3dc952dd91d555ee572ec6ec9051a08961a4d228
locoda <locoda@foxmail.com>
2017-06-24 20:34:51 +0800

    update supervisor file and add multi-threading to bot

--- wechat-bot/wxbot.py
+++ wechat-bot/wxbot.py
@@ -7,6 +7,7 @@ import unicodedata
 import re
 import time
 import logging
+import threading
 from tempfile import NamedTemporaryFile
 
 logging.basicConfig()
@@ -14,12 +15,20 @@ bot = Bot(cache_path=True, console_qr=True,)
 bot.enable_puid()
 extensions = ['.jpg', '.png', '.gif']
 
-@bot.register(bot.self, except_self=False)
+def send_online_notification(name):
+    my_friend = ensure_one(bot.search(name))
+    while True:
+        my_friend.send('I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime()))
+        time.sleep(3600)
+
+
+@bot.register()
 def reply(msg):
 	if msg.text == '1':
 		return 'I\'m Still Alive!! ' + time.strftime('%y/%m/%d-%H:%M:%S', time.localtime())
 	else:
 		return tuling_reply(content, msg.sender.puid)
+
 
 @bot.register(Group, TEXT)
 def group_msg(msg):
@@ -34,7 +43,7 @@ def group_msg(msg):
 				tmp.flush()
 				media_id = bot.upload_file(tmp.name)
 				tmp.close()
-				
+
 				msg.reply_image('.gif', media_id=media_id)
 			except Exception as error:
 				print error
@@ -45,5 +54,10 @@ def group_msg(msg):
 			except Exception as error:
 				print error
 				msg.reply(tuling_reply(content, msg.member.puid))
+
+positiveSendingThread = threading.Thread(target=send_online_notification,
+    args=(u'乙醚。',))
+positiveSendingThread.setDaemon(True)
+positiveSendingThread.start()
 
 embed()
